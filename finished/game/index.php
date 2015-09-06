44eae7252f9927d78ad87acc94400b531a81efea
CPekko <eirik_fosse2@hotmail.com>
2015-09-06 20:52:00 +0200

    List finished game and view game info.

---
+++ finished/game/index.php
@@ -0,0 +1,171 @@
+<?php
+date_default_timezone_set("Europe/Oslo");
+require_once('../../partials/classes.php');
+require_once('../../partials/funcs.php');
+include('../../views/partials/head.php');
+if(isset($_GET['id'])){
+	$game = getGame($_GET['id']);
+    $bestGame = getBestGame($game->getPlayer());
+    $average = round(getAverageGame($game->getPlayer()));
+	?>
+	 <div class="container body-content">
+		<section>
+		    <h3 class="player-name" style="margin: 40px 0"><img src="<?echo $game->getPlayer()->getImage()?>"><? echo $game->getPlayer()->getName() ?>'s Game</h3>
+			<div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Game finished in <b><?echo sizeof($game->getThrows())?></b> throws
+                            <span class="pull-right">Finished <?echo date('d.m.Y k\l. H:m',strtotime($game->getTimeFinished()));?></span>
+                        </div>
+                        <div class="panel-body">
+                            <table class="table game">
+                                <thead>
+                                    <tr>
+                                        <th>Target</th>
+                                        <? for ($i = 1; $i <= 20; $i++){ ?>
+                                             <th class="text-center"><? echo $i ?></th>
+                                        <?}?>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Throws</td>
+                                    <!--<tr ng-if="vm.bestGame" game-stats header-content="'best game'" game="vm.bestGame" cumulative-throws="showCumulativeThrows"></tr>
+                                    <tr game-stats header-content="'current game'" game="vm.game" cumulative-throws="showCumulativeThrows"></tr>-->
+                                    <?
+                                        $throwList = getThrowList($game);
+                                        $bestThrowList = getThrowList($bestGame);
+                                        $highchartArray = array();
+                                        $highchartArray2 = array();
+                                        $highchartArray3 = array();
+                                        for ($i = 1; $i <= 20; $i++){
+                                            $highchartArray[] = array($i, $throwList[$i]);
+                                            $highchartArray2[] = array($i, $throwList[$i] + $highchartArray2[$i-2][1]);
+                                            ?> <td class="text-center" id="target-<? echo $i?>"><? echo $throwList[$i] . "</td>";
+                                        }
+                                        ?></tr><tr><td>Best Game</td> <?
+                                        for ($i = 1; $i <= 20; $i++){
+                                            $highchartArray3[] = array($i, $bestThrowList[$i] + $highchartArray3[$i-2][1]);
+                                            ?> <td class="text-center" id="target-<? echo $i?>"><? echo $bestThrowList[$i] . "</td>";
+                                        }
+                                    ?>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">This Game vs Best Game</div>
+                        <div class="panel-body">
+                            <highchart id="totalThrows"></highchart>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Throws per Target</div>
+                        <div class="panel-body">
+                            <highchart id="hitsPerTarget"></highchart>
+                        </div>
+                    </div>
+                </div>
+            </div>
+		</section>
+	</div>
+<?}
+include('../../views/partials/footer.php'); ?>
+<script src="/eiriknf/dart/scripts/highcharts.js"></script>
+<script>
+$( document ).ready(function() {
+    $('#hitsPerTarget').highcharts({
+        title: true,
+        credits: {
+            enabled: false
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            formatter: function () {
+                if(this.y === 1) return ' Used <b>' + this.y + '</b> throw on <b>' + this.x + '</b>';
+                return ' Used <b>' + this.y + '</b> throws on <b>' + this.x + '</b>';
+            }
+        },
+        xAxis: {
+            title: {
+                text: 'Target'
+            },
+            categories: ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'],
+            labels:{
+                step: 1
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Throws'
+            }
+        },
+        loading: true,
+        series: [{
+            data: <?echo json_encode($highchartArray)?>
+        }]
+    });
+
+    $('#totalThrows').highcharts({
+        title: true,
+        credits: {
+            enabled: false
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        tooltip: {
+            headerFormat:  '',
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> throws to <b>{point.x}</b><br/>',
+            shared: true
+        },
+        plotOptions: {
+            line:{
+                marker: {
+                    enabled: false
+                }
+            }
+        },
+        xAxis: {
+            title: {
+                text: 'Target'
+            },
+            categories: ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'],
+            labels:{
+                step: 1
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Throws'
+            },
+            min: 0,
+            plotLines: [{
+                value: <?echo $average?>,
+                color: 'red',
+                width: 2,
+                label: {
+                    text: 'Average Game - <?echo $average?>',
+                    y: 16
+                },
+                verticalAlign: 'bottom',
+                zIndex: 5
+            }]
+        },
+        loading: true,
+        series: [{
+            name: 'This Game',
+            data: <?echo json_encode($highchartArray2)?>
+        }, {
+            name: 'Best Game',
+            data: <?echo json_encode($highchartArray3)?>
+        }]
+    });
+});
+</script>
---
+++ finished/index.php
@@ -0,0 +1,47 @@
+<?php
+date_default_timezone_set("Europe/Oslo");
+require_once('../partials/classes.php');
+require_once('../partials/funcs.php');
+include('../views/partials/head.php');
+$finishedGames = getFinishedGames();
+$totalThrowCount = getTotalThrowCount();
+?>
+ <div class="container body-content">
+	<section>
+	    <style type="text/css">
+	        .best-game td {
+	            background-color: #eee;
+	        }
+	    </style>
+		
+	    <h2>Finished Games</h2>
+	    <p style="font-style: italic">Total of <?echo number_format(sizeof($finishedGames), 0, ",", " ")?> games played and <?echo number_format($totalThrowCount, 0, ",", " ")?> darts thrown. Click on column to sort.</p>
+
+	    <table class="table finished">
+	        <thead>
+	            <tr>
+	                <th onclick="sortTable(1)">Player</th>
+	                <th onclick="sortTable(2)">Started</th>
+	                <th onclick="sortTable(3)">Finished</th>
+	                <th onclick="sortTable(4)" class="text-center">Throws</th>
+	                <th style="text-align: right">View game</th>
+	            </tr>
+	        </thead>
+	        <tbody>
+	        	<?
+	        	foreach ($finishedGames as $game){
+	        	?>
+		            <tr>
+		                <td class="name"><? echo $game->getPlayer()->getName(); ?></td>
+		                <td><span class="started" data-timestamp="<?echo strtotime($game->getTimeStarted())?>" data-time="<? echo $game->getTimeStarted(); ?>"></span></td>
+		                <td><span class="finished" data-timestamp="<?echo strtotime($game->getTimeFinished())?>" data-time="<? echo $game->getTimeFinished(); ?>"></span></td>
+		                <td class="text-center" style="font-weight:bold" id="game-<?echo $game->getGameId()?>" data-player-id="<?echo $game->getPlayer()->getPlayerId()?>"><? echo $game->getNumThrows(); ?></td>
+		                <td style="text-align: right; font-weight: normal;"><a href="game/?id=<?echo $game->getGameId()?>">View game</a></td>
+		            </tr>
+		        <?}?>
+	        </tbody>
+	    </table>
+	</section>
+</div>
+<? include('../views/partials/footer.php'); ?>
+<script src="../scripts/finished-games.js"></script>
